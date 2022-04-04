2d1e6a09b6a8f1c150fd60ac80e30fbc2f8b8359
Rob Blankson <robert.blankson@hudl.com>
2022-04-04 03:06:58 -0700

    Update zimcontainer.js

--- src/components/zimcontainer.js
+++ src/components/zimcontainer.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react"
-import "../../node_modules/@danzen/createjs/create"
-import "../../node_modules/zimjs/zim"
+import { useRef, useState, useEffect } from "react"
 
-// CreateJS (create.js) and ZIM (zim.js) should be imported in the main script... ...but how?
+import makeCreateJS from "createjs/builds/1.0.0/createjs.js";
+window.createjs = makeCreateJS();
+const makeZIM = require('zimjs');
+const zim = makeZIM(true);
 
 export default class ZIMContainer extends Component {
   componentDidMount() {
     // or set size in React and do not add dimensions here
 
-    const frame = new Frame(this.zimRootElement.id, 1024, 768)
+    const frame = new zim.Frame(this.zimRootElement.id, 1024, 768)
 
     frame.on("ready", () => {
       const stage = frame.stage
@@ -18,7 +20,7 @@ export default class ZIMContainer extends Component {
       const stageH = frame.height
 
       // ZIM code here
-      new Circle(60, "red").center().drag();
+      new zim.Circle(60, "red").center().drag();
 
       stage.update()
     })
